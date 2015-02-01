0353a66188af0a497dca7d70295019245a8c09c3
JessaMaeA <JM.atutubo@yahoo.com>
2015-02-01 09:32:01 +0800

    this is a text or a message to you repo

--- main.py
+++ main.py
@@ -9,3 +9,4 @@ FPS = 10 # frames per second to update the screen
 WINDOWWIDTH = 640 # width of the program's window, in pixels
 WINDOWHEIGHT = 480 # height in pixels
 SPACESIZE = 50 # width & height of each space on the board, in pixels
+BOARDWIDTH = 8 # how many columns of spaces on the game board
