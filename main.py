daf5bc31ff269d3c45ff72d07a002cb3587e5afd
JessaMaeA <JM.atutubo@yahoo.com>
2015-02-22 14:20:22 +0800

    update main.py

--- main.py
+++ main.py
@@ -469,3 +469,5 @@ def getComputerMove(board, computerTile):
 	# Given a board and the computer's tile, determine where to
 	# move and return that move as a [x, y] list.
  possibleMoves = getValidMoves(board, computerTile)
+ 
+	# randomize the order of the possible moves
